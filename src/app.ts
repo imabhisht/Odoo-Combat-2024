5df4799beed16de6e0cea3cf9d660b35f42a89a7
Prayag <prayagbhatt2003@gmail.com>
2024-06-29 13:23:21 +0530

    added location entities

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,38 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { userRouter } from "./routes/user.route";
 import * as dotenv from "dotenv";
+import connectDB from "./db/conn";
+
 dotenv.config();
 
 const app = express();
+
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
+
 const port = process.env.PORT || 5000;
+
+connectDB()
+	.then(() => {
+		app.listen(port, () => {
+			console.log(
+				`Server is running at http://localhost:${port}`
+			);
+		});
+	})
+	.catch((err) => {
+		console.log("MongoDB failed to connect ...", err);
+	});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 app.use("/users", userRouter);
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello, World!");
-});
-
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
 });

149ec680aa70f9b37263c6234c8e90cfea126139
Aniket-Patel-swg <patelaniket1207@gmail.com>
2024-06-29 14:45:54 +0530

    added heatmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MarkersPage from './Page/MarkersPage'
-import { Routes, Route } from 'react-router-dom'
-  
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import MarkersPage from './Page/MarkersPage';
+import HeatMap from './components/HeatMap';
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const heatmapData = [
+    [51.505, -0.09, 0.5],  // Latitude, Longitude, Intensity
+    [51.515, -0.1, 0.3],
+    [51.52, -0.12, 0.8]
+  ];
 
   return (
     <>
       <Routes>
         <Route path="/Location" element={<MarkersPage />} />
+        <Route path='/HeatMap' element={<HeatMap />} />
       </Routes>
 
     </>

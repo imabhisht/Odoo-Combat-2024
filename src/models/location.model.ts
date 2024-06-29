00d74fbfdc42f9b5e730a812d2a2371e85a6912f
Prayag <prayagbhatt2003@gmail.com>
2024-06-29 13:22:25 +0530

    add: location model

--- src/models/location.model.ts
+++ src/models/location.model.ts
@@ -1,18 +1,16 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface ILocation extends Document {
-	report_id: mongoose.Types.ObjectId;
+	userId: mongoose.Types.ObjectId;
 	latitude: number;
 	longitude: number;
-	crime_type: string;
 	timestamp: Date;
 }
 
 const LocationSchema: Schema = new Schema({
-	report_id: { type: Schema.Types.ObjectId, ref: "CrimeReport" },
+	userId: { type: Schema.Types.ObjectId, ref: "User" },
 	latitude: { type: Number, required: true },
 	longitude: { type: Number, required: true },
-	crime_type: { type: String, required: true },
 	timestamp: { type: Date, required: true },
 });
 
